ef17767a250b81a6abcc922d32c291fe3e865e79
daryna-budz <darynabudz@gmail.com>
2024-06-06 13:26:54 -0400

    fully re sponsive main page done, all media queries added, burger menu i mplemented using javascript for small devices

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function showMenu(){
+    const menu = document.getElementById("fullscreenMenu");
+    menu.classList.toggle('show');
+}
+
+function closeMenu(){
+    const menu = document.getElementById("fullscreenMenu");
+    menu.classList.remove('show');
+}
